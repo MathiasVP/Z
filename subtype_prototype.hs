eb3a1aedceded6fc8f628fccd20a6a654580884d
Mathias VP <mathias.vp@hotmail.com>
2015-07-16 01:27:09 +0200

    Added IO monad where we need to call unify

--- subtype_prototype.hs
+++ subtype_prototype.hs
@@ -3,24 +3,40 @@ import qualified Data.Graph.Inductive.Dot as Dot
 import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Set as Set
+import Data.Unique as U
+import Control.Monad
 import Data.Ord
-import Debug.Trace as Debug
 
 data Type
   = Name String [(String, Type)]
+  | Array Type
+  | Set Type
   | Tuple [Type]
   | Union Type Type
   | Arrow Type Type
   | Record [(String, Type)]
+  | TypeVar Unique
+  | AllOf [Type]
+  | Error
   deriving (Show, Ord, Eq)
+  
+instance Show Unique where
+  show u = "Unique " ++ show (hashUnique u)
+
+-- TypeVar
 
 data TypeTag
   = NameTag String
   | NameDefTag String [String]
+  | ArrayTag
+  | SetTag
   | TupleTag
   | UnionTag
   | ArrowTag
   | RecordTag
+  | TypeVarTag Unique
+  | AllOfTag
+  | ErrorTag
   deriving Show
 
 data GraphEdge
@@ -31,6 +47,13 @@ data GraphEdge
   deriving (Show, Ord, Eq)
              
 type TypeGraph = (Map.Map String G.Node, G.Gr TypeTag GraphEdge)
+type Substitution = Map.Map U.Unique Type
+type Env = Map.Map String Type
+type InstType = (G.Node, Map.Map String G.Node)
+type Visited = Set.Set (InstType, InstType)
+
+unify :: Type -> Type -> Substitution -> IO (Type, Substitution)
+unify = undefined
 
 empty = (Map.fromList [("Int", 0)],
          G.mkGraph [(0, NameTag "Int")] [])
@@ -58,6 +81,11 @@ lab (map, gr) node = G.lab gr node
 lsuc :: G.Node -> TypeGraph -> [(G.Node, GraphEdge)]
 lsuc node (_, gr) = G.lsuc gr node
 
+foldlWithKeyM :: Monad m => (a -> k -> b -> m a) -> a -> Map.Map k b -> m a
+foldlWithKeyM f acc = Map.foldlWithKey f' (return acc) 
+    where
+        f' ma k b = ma >>= \a -> f a k b
+
 type2graph :: Type -> Map.Map String G.Node -> TypeGraph -> (TypeGraph, G.Node)
 type2graph (Name name args) mapt gr =
   case Map.lookup name mapt of
@@ -75,6 +103,14 @@ type2graph (Tuple tys) mapt gr =
   where (gr', node) = newNode TupleTag gr
         f gr (n, ty) = insertEdge node node' (NumEdge n) gr'
           where (gr', node') = type2graph ty mapt gr
+type2graph (Array t) mapt gr = (gr''', node)
+  where (gr', node) = newNode ArrayTag gr
+        (gr'', node') = type2graph t mapt gr'
+        gr''' = insertEdge node node' (NumEdge 0) gr''
+type2graph (Set t) mapt gr = (gr''', node)
+  where (gr', node) = newNode SetTag gr
+        (gr'', node') = type2graph t mapt gr'
+        gr''' = insertEdge node node' (NumEdge 0) gr''
 type2graph (Union t1 t2) mapt gr = (gr''', node)
   where (gr', node) = newNode UnionTag gr
         (gr1, node1) = type2graph t1 mapt gr'
@@ -92,6 +128,16 @@ type2graph (Record fields) mapt gr = (gr'', node)
         gr'' = List.foldl' f gr' fields
         f gr (name, ty) = insertEdge node node' (RecordEdge name) gr'
           where (gr', node') = type2graph ty mapt gr
+type2graph (AllOf tys) mapt gr = (gr'', node)
+  where (gr', node) = newNode AllOfTag gr
+        gr'' = List.foldl' f gr' (List.zip tys [0..])
+        f gr (ty, n) = insertEdge node node' (NumEdge n) gr'
+          where (gr', node') = type2graph ty mapt gr
+type2graph (TypeVar u) mapt gr = newNode (TypeVarTag u) gr
+type2graph Error mapt gr = newNode ErrorTag gr
+
+graph2type :: InstType -> Env -> TypeGraph -> Type
+graph2type (n, inst) env gr = undefined
 
 translateDecl :: String -> Type -> [String] -> TypeGraph -> (TypeGraph, G.Node)
 translateDecl name ty args gr =
@@ -104,92 +150,113 @@ translateDecl name ty args gr =
             f name (gr, mapt) = (gr', Map.insert name node mapt)
               where (gr', node) = newNode (NameTag name) gr
             (gr'''', n) = type2graph ty mapt gr'''
-            
-type InstType = (G.Node, Map.Map String G.Node)
-type Visited = Set.Set (InstType, InstType)
 
-subtype :: Visited -> InstType -> InstType -> TypeGraph -> (Bool, Visited)
-subtype visited (n1, inst1) (n2, inst2) gr =
-  case (Debug.trace ("n1: " ++ show n1 ++ show (Map.toList inst1)) (lab gr n1), Debug.trace ("n2: " ++ show n2 ++ show (Map.toList inst2) ++ "\n") (lab gr n2)) of
+subtype :: Visited -> InstType -> InstType -> Env -> Substitution -> TypeGraph -> IO (Bool, Visited, Substitution)
+subtype visited (n1, inst1) (n2, inst2) env subst gr =
+  case (lab gr n1, lab gr n2) of
+    (Just ErrorTag, Just _) -> return (True, visited, subst)
+    (Just _, Just ErrorTag) -> return (True, visited, subst)
     (Just (NameDefTag s1 args), Just _) ->
       case lsuc n1 gr of
         [(n, DefEdge)]
           | Map.size inst1 == List.length args ->
-            if Set.member ((n, inst1), (n2, inst2)) visited then (True, visited)
-            else subtype (Set.insert ((n, inst1), (n2, inst2)) visited) (n, inst1) (n2, inst2) gr
+            if Set.member ((n, inst1), (n2, inst2)) visited then return (True, visited, subst)
+            else subtype (Set.insert ((n, inst1), (n2, inst2)) visited) (n, inst1) (n2, inst2) env subst gr
     (Just _, Just (NameDefTag s2 args)) ->
       case lsuc n2 gr of
         [(n, DefEdge)]
           | Map.size inst2 == List.length args ->
-            if Set.member ((n1, inst1), (n, inst2)) visited then (True, visited)
-            else subtype (Set.insert ((n1, inst1), (n, inst2)) visited) (n1, inst1) (n, inst2) gr 
+            if Set.member ((n1, inst1), (n, inst2)) visited then return (True, visited, subst)
+            else subtype (Set.insert ((n1, inst1), (n, inst2)) visited) (n1, inst1) (n, inst2) env subst gr 
     (Just _, Just (NameTag s2)) ->
       case Map.lookup s2 inst2 of
         Just n ->
-          if Set.member ((n1, inst1), (n, Map.empty)) visited then (True, visited)
-          else subtype (Set.insert ((n1, inst1),(n, Map.empty)) visited) (n1, inst1) (n, Map.empty) gr
+          if Set.member ((n1, inst1), (n, Map.empty)) visited then return (True, visited, subst)
+          else subtype (Set.insert ((n1, inst1),(n, Map.empty)) visited) (n1, inst1) (n, Map.empty) env subst gr
         Nothing ->
           case lsuc n2 gr of
-            [] -> (Set.member ((n1, inst1), (n2, inst2)) visited, visited)
+            [] -> return (Set.member ((n1, inst1), (n2, inst2)) visited, visited, subst)
             sucs@((n, _):_) ->
-              if Set.member ((n1, inst1),(n, inst2')) visited then (True, visited)
-              else subtype (Set.insert ((n1, inst1),(n, inst2')) visited) (n1, inst1) (n, inst2') gr
+              if Set.member ((n1, inst1),(n, inst2')) visited then return (True, visited, subst)
+              else subtype (Set.insert ((n1, inst1),(n, inst2')) visited) (n1, inst1) (n, inst2') env subst gr
               where inst2' = Map.fromList $ List.map (\(_, InstEdge s n) -> (s, n)) sucs
     (Just (NameTag s1), Just _) ->
       case Map.lookup s1 inst1 of
         Just n ->
-          if Set.member ((n, inst1),(n2, Map.empty)) visited then (True, visited)
-          else subtype (Set.insert ((n, inst1), (n2, Map.empty)) visited) (n, inst1) (n2, Map.empty) gr
+          if Set.member ((n, inst1),(n2, Map.empty)) visited then return (True, visited, subst)
+          else subtype (Set.insert ((n, inst1), (n2, Map.empty)) visited) (n, inst1) (n2, Map.empty) env subst gr
         Nothing ->
           case lsuc n1 gr of
-            [] -> (Set.member ((n1, inst1), (n2, inst2)) visited, visited)
+            [] -> return (Set.member ((n1, inst1), (n2, inst2)) visited, visited, subst)
             sucs@((n, _):_) ->
-              if Set.member ((n, inst1'),(n2, inst2)) visited then (True, visited)
-              else subtype (Set.insert ((n, inst1'),(n2, inst2)) visited) (n, inst1') (n2, inst2) gr
+              if Set.member ((n, inst1'),(n2, inst2)) visited then return (True, visited, subst)
+              else subtype (Set.insert ((n, inst1'),(n2, inst2)) visited) (n, inst1') (n2, inst2) env subst gr
               where inst1' = Map.fromList $ List.map (\(_, InstEdge s n) -> (s, n)) sucs
+    (Just ArrayTag, Just ArrayTag) ->
+      case (lsuc n1 gr, lsuc n2 gr) of
+        ([(n1, _)], [(n2, _)]) ->
+          subtype visited (n1, inst1) (n2, inst2) env subst gr
+    (Just SetTag, Just SetTag) ->
+      case (lsuc n1 gr, lsuc n2 gr) of
+        ([(n1, _)], [(n2, _)]) ->
+          subtype visited (n1, inst1) (n2, inst2) env subst gr
     (Just UnionTag, Just UnionTag) ->
-      ((b11 || b12) && (b21 || b22), v4)
-      where [(a, _), (b, _)] = lsuc n1 gr
-            [(c, _), (d, _)] = lsuc n2 gr
-            (b11, v1) = subtype visited (a, inst1) (c, inst2) gr
-            (b12, v2) = subtype v1 (a, inst1) (d, inst2) gr
-            (b21, v3) = subtype v2 (b, inst1) (c, inst2) gr
-            (b22, v4) = subtype v3 (b, inst1) (d, inst2) gr
-    (Just _, Just UnionTag) -> (b1 || b2, v2)
-      where [(a, _), (b, _)] = lsuc n2 gr
-            (b1, v1) = subtype visited (n1, inst1) (a, inst2) gr
-            (b2, v2) = subtype v1 (n1, inst1) (b, inst2) gr
+      let [(a, _), (b, _)] = lsuc n1 gr
+          [(c, _), (d, _)] = lsuc n2 gr
+      in do (b11, v1, subst1) <- subtype visited (a, inst1) (c, inst2) env subst gr
+            (b12, v2, subst2) <- subtype v1 (a, inst1) (d, inst2) env subst1 gr
+            (b21, v3, subst3) <- subtype v2 (b, inst1) (c, inst2) env subst2 gr
+            (b22, v4, subst4) <- subtype v3 (b, inst1) (d, inst2) env subst3 gr
+            return ((b11 || b12) && (b21 || b22), v4, subst4)
+    (Just _, Just UnionTag) ->
+      let [(a, _), (b, _)] = lsuc n2 gr
+      in do (b1, v1, subst1) <- subtype visited (n1, inst1) (a, inst2) env subst gr
+            (b2, v2, subst2) <- subtype v1 (n1, inst1) (b, inst2) env subst1 gr
+            return (b1 || b2, v2, subst2)
     (Just UnionTag, Just _) ->
-      (b1 && b2, v2)
-      where [(a, _), (b, _)] = lsuc n1 gr
-            (b1, v1) = subtype visited (a, inst1) (n2, inst2) gr
-            (b2, v2) = subtype v1 (b, inst1) (n2, inst2) gr
+      let [(a, _), (b, _)] = lsuc n1 gr
+      in do (b1, v1, subst1) <- subtype visited (a, inst1) (n2, inst2) env subst gr
+            (b2, v2, subst2) <- subtype v1 (b, inst1) (n2, inst2) env subst1 gr
+            return (b1 && b2, v2, subst2)
+    (Just AllOfTag, Just _) ->
+      let (nodes, _) = List.unzip $ lsuc n1 gr
+      in foldM f (True, visited, subst) nodes
+      where f (b, visited, subst) node =
+              if b then subtype visited (node, inst1) (n2, inst2) env subst gr
+              else return (False, visited, subst)
+    (Just _, Just AllOfTag) ->
+      let (nodes, _) = List.unzip $ lsuc n2 gr
+      in foldM f (True, visited, subst) nodes
+      where f (b, visited, subst) node =
+              if b then subtype visited (n1, inst1) (node, inst2) env subst gr
+              else return (False, visited, subst)
     (Just TupleTag, Just TupleTag)
       | List.length sucs1 == List.length sucs2 ->
-        List.foldl' f (True, visited) (List.zip sucs1 sucs2)
-      | otherwise -> (False, visited)
+        foldM f (True, visited, subst) (List.zip sucs1 sucs2)
+      | otherwise -> return (False, visited, subst)
       where sucs1 = List.map fst $ List.sort $ lsuc n1 gr
             sucs2 = List.map fst $ List.sort $ lsuc n2 gr
-            f (b, visited) (n1, n2) =
-                if b then (b', visited')
-                else (False, visited)
-                where (b', visited') = subtype visited (n1, inst1) (n2, inst2) gr
-    (Just ArrowTag, Just ArrowTag) -> (b1 && b2, v2)
-      where [(dom1, _), (cod1, _)] = List.sort $ lsuc n1 gr
-            [(dom2, _), (cod2, _)] = List.sort $ lsuc n2 gr
-            (b1, v1) = subtype visited (dom1, inst1) (dom2, inst2) gr
-            (b2, v2) = subtype v1 (cod2, inst2) (cod1, inst1) gr
+            f (b, visited, subst) (n1, n2) =
+              if b then subtype visited (n1, inst1) (n2, inst2) env subst gr
+              else return (False, visited, subst)
+    (Just ArrowTag, Just ArrowTag) -> 
+      let [(dom1, _), (cod1, _)] = List.sort $ lsuc n1 gr
+          [(dom2, _), (cod2, _)] = List.sort $ lsuc n2 gr
+      in do (b1, v1, subst1) <- subtype visited (dom1, inst1) (dom2, inst2) env subst gr
+            (b2, v2, subst2) <- subtype v1 (cod2, inst2) (cod1, inst1) env subst1 gr
+            return (b1 && b2, v2, subst2)
     (Just RecordTag, Just RecordTag) ->
-      Map.foldrWithKey g (True, visited) fields1
+      foldlWithKeyM g (True, visited, subst) fields1
       where fields1 = Map.fromList $ List.map f $ lsuc n1 gr
             fields2 = Map.fromList $ List.map f $ lsuc n2 gr
             f (n, RecordEdge s) = (s, n)
-            g s n (b, visited) =
-              if b then (True, visited)
-              else case Map.lookup s fields2 of
-                    Just n' -> subtype visited (n, inst1) (n', inst2) gr
-                    Nothing -> (False, visited)
-    (Just _, Just _) -> (False, visited)
+            g (b, visited, subst) s n =
+              if b then
+                case Map.lookup s fields2 of
+                  Just n' -> subtype visited (n, inst1) (n', inst2) env subst gr
+                  Nothing -> return (False, visited, subst)
+              else return (False, visited, subst)
+    (Just _, Just _) -> return (False, visited, subst)
 
 main :: IO ()
 main = do
@@ -198,4 +265,6 @@ main = do
   let (gr1, _) = translateDecl "A" ty1 ["T"] empty
   let (gr2, _) = translateDecl "B" ty2 ["T"] gr1
   putStrLn $ Dot.showDot $ Dot.fglToDot (snd gr2)
-  print $ fst $ subtype Set.empty (1, Map.fromList [("T", 0)]) (6, Map.fromList [("T", 0)]) gr2
+  let env = Map.fromList [("A", ty1), ("B", ty2)]
+  (b, _, _) <- subtype Set.empty (1, Map.fromList [("T", 0)]) (6, Map.fromList [("T", 0)]) env Map.empty gr2
+  print $ b
